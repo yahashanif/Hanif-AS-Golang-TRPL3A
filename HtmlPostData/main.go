45c380654f4bd1ce6ebf8cf134c483ffcb24ca22
hanif0209 <52150885+hanif0209@users.noreply.github.com>
2020-10-30 22:28:59 +0700

    Tes

--- HtmlPostData/main.go
+++ HtmlPostData/main.go
@@ -12,6 +12,6 @@ func main() {
 	http.HandleFunc("/", fn.RouteIndexGet)
 	http.HandleFunc("/process", fn.RouteSubmitPost)
 
-	fmt.Println("server started at localhost:9000")
+	fmt.Println("server started at : localhost:9000")
 	http.ListenAndServe(":9000", nil)
 }
